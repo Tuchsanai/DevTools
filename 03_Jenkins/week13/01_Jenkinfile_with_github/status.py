d33317ff2aede38e5997fa8f00d206460c8bc391
tuchsanai <tuchsanai@gmail.com>
2024-02-27 09:45:53 +0700

    aa

---
+++ 03_Jenkins/week13/01_Jenkinfile_with_github/status.py
@@ -0,0 +1,16 @@
+import os
+
+def main():
+    # Displaying status
+    print("System Status:")
+    print("---------------")
+    print("Operating System:", os.name)
+    print("\n")
+    print("User:", os.getlogin())
+    print("\n")
+    print("Environment Variables:", os.environ)
+    print("\n")
+    print("finished.")
+
+if __name__ == "__main__":
+    main()
